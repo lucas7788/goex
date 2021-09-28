5185908ea4614705e24b419488927bc5a8d03d87
lucas7788 <sishsh@163.com>
2021-09-28 19:44:16 +0800

    add SUSHI

--- CurrencyPair.go
+++ CurrencyPair.go
@@ -65,6 +65,7 @@ var (
 	CRV     = Currency{"CRV", ""}
 	ALGO    = Currency{"ALGO", ""}
 	WING    = Currency{"WING", ""}
+	SUSHI    = Currency{"SUSHI", ""}
 
 	//currency pair
 	BTC_KRW = CurrencyPair{CurrencyA: BTC, CurrencyB: KRW, AmountTickSize: 2, PriceTickSize: 1}
@@ -144,7 +145,7 @@ var (
 	LTC_ETH = CurrencyPair{CurrencyA: LTC, CurrencyB: ETH, AmountTickSize: 2, PriceTickSize: 4}
 
 	WING_USDT = CurrencyPair{CurrencyA: WING, CurrencyB: USDT, AmountTickSize: 2, PriceTickSize: 3}
-
+	SUSHI_USDT = CurrencyPair{CurrencyA: SUSHI, CurrencyB: USDT, AmountTickSize: 2, PriceTickSize: 3}
 	UNKNOWN_PAIR = CurrencyPair{CurrencyA: UNKNOWN, CurrencyB: UNKNOWN}
 )
 
